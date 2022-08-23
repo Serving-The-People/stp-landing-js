aa87e4688bb6eca2be654f5a639560c771e7cefa
danny de <danielde666@users.noreply.github.com>
2022-08-23 10:36:06 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
+
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div classNameName={styles.container}>
       <Head>
 	      <title>Serving The People - The Social Platform of the Future, Today</title>	<meta name="description" content="The Social Platform of the Future, Today" />
         <meta property="og:locale" content="en_US" />
@@ -30,55 +31,55 @@ export default function Home() {
       </Head>
 
       
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" ></script>
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" ></Script>
 
   
       <>
-      <header class="p-3">
-          <div class="container">
-            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-              <a href="" class="d-flex align-items-center mb-2 mb-lg-0  text-decoration-none">
-                <img class="bi me-2" width="36" height="30" role="img" src="http://home.stp.world/stp-home-logo.png"/>
+      <header className="p-3">
+          <div className="container">
+            <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+              <a href="" className="d-flex align-items-center mb-2 mb-lg-0  text-decoration-none">
+                <Image className="bi me-2" width={36} height={30} role="img" src="http://home.stp.world/stp-home-logo.png"/>
               </a>
-              <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0 fw-bold">
-                <li><a href="https://docs.stp.world/" class="nav-link px-2 ">Docs</a></li>
-                <li><a href="https://stp.world/" class="nav-link px-2 ">Blog</a></li>
-                <li><a href="https://stp.world/radio" class="nav-link px-2 ">Radio</a></li>
-                <li><a href="https://shop.stp.world/" class="nav-link px-2 ">Shop</a></li>
+              <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0 fw-bold">
+                <li><a href="https://docs.stp.world/" className="nav-link px-2 ">Docs</a></li>
+                <li><a href="https://stp.world/" className="nav-link px-2 ">Blog</a></li>
+                <li><a href="https://stp.world/radio" className="nav-link px-2 ">Radio</a></li>
+                <li><a href="https://shop.stp.world/" className="nav-link px-2 ">Shop</a></li>
               </ul>
-              <div class="text-end">
-                <button type="button" class="btn me-2" href="https://stp.world/contact/">Contact Team</button>
+              <div className="text-end">
+                <button type="button" className="btn me-2" href="https://stp.world/contact/">Contact Team</button>
                 
-                <button type="button" class="btn btn-secondary" href="https://discord.gg/nhqyng5wQ9">Join Discord</button>
+                <button type="button" className="btn btn-secondary" href="https://discord.gg/nhqyng5wQ9">Join Discord</button>
               </div>
             </div>
           </div>
       </header>  
 
-      <main class="container">
-        <div class="text-center">
-          <h1 class="display-3 fw-bold mt-5">Welcome to<br></br>
+      <main className="container">
+        <div className="text-center">
+          <h1 className="display-3 fw-bold mt-5">Welcome to<br></br>
           Serving the People!</h1>
-          <div class="col-lg-12 mx-auto">
-            <p class=" mb-5">Serving the People is a group of creatives and technologists<br></br>
+          <div className="col-lg-12 mx-auto">
+            <p className=" mb-5">Serving the People is a group of creatives and technologists<br></br>
               building the future of creativity, collaboration,<br></br>
               and communication.</p>
           </div>
-          <h3 class="display-6 fw-bold pt-4">Join our community</h3>
-          <p class=" mb-4">Currently <b>1,686 creators</b><br></br>
+          <h3 className="display-6 fw-bold pt-4">Join our community</h3>
+          <p className=" mb-4">Currently <b>1,686 creators</b><br></br>
           <b>2,055 Seeds</b> on <a href="https://opensea.io/collection/seeds-luciensmith">OpenSea</a></p>
 
-          <div class="mx-auto mb-4" style="max-width:420px;">
-            <form class="row row-cols-xs-auto g-3 align-items-center">
-              <div class="col-6">
-                <label class="visually-hidden" for="inlineFormInputGroupUsername">Quantity</label>
-                <div class="input-group">
-                  <input type="number" class="form-control" id="inlineFormInputGroupUsername" placeholder="1 or More"
+          <div className="mx-auto mb-4" style="max-width:420px;">
+            <form className="row row-cols-xs-auto g-3 align-items-center">
+              <div className="col-6">
+                <label className="visually-hidden" for="inlineFormInputGroupUsername">Quantity</label>
+                <div className="input-group">
+                  <input type="number" className="form-control" id="inlineFormInputGroupUsername" placeholder="1 or More"
                   />
                 </div>
               </div>
-              <div class="col-6">
-                <button type="submit" class="btn btn-primary btn-block w-100">Mint a Seed</button>
+              <div className="col-6">
+                <button type="submit" className="btn btn-primary btn-block w-100">Mint a Seed</button>
               </div>
             </form>
           </div>
@@ -87,16 +88,18 @@ export default function Home() {
           </p>
         </div>
       </main>
-      <footer class="d-flex flex-wrap justify-content-between align-items-end py-3 my-4 container">
-        <div class="col-md-4 d-flex align-items-center">
-          <span class=" mb-md-0">© 2022 Serving The People, Inc.</span>
+      <footer className="d-flex flex-wrap justify-content-between align-items-end py-3 my-4 container">
+        <div className="col-md-4 d-flex align-items-center">
+          <span className=" mb-md-0">© 2022 Serving The People, Inc.</span>
         </div>
-        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-          <li class="ms-3"><a href="https://opensea.io/collection/seeds-luciensmith">
-          <i class="bi bi-opensea"><img style="vertical-align:-.18em;"width="18" height="18" src="opensea.svg"/></i></a></li>
-          <li class="ms-3"><a href="https://www.instagram.com/servingthepeople/"><i class="bi bi-instagram"></i></a></li>
-          <li class="ms-3"><a href="https://twitter.com/STPcommunity"><i class="bi bi-twitter"></i></a></li>
-          <li class="ms-3"><a href="https://discord.gg/nhqyng5wQ9"><i class="bi bi-discord"></i></a></li>
+        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+          <li className="ms-3"><a href="https://opensea.io/collection/seeds-luciensmith">
+          <i className="bi bi-opensea">
+            <Image style="vertical-align:-.18em;" width={18} height={18} src="opensea.svg"/>
+            </i></a></li>
+          <li className="ms-3"><a href="https://www.instagram.com/servingthepeople/"><i className="bi bi-instagram"></i></a></li>
+          <li className="ms-3"><a href="https://twitter.com/STPcommunity"><i className="bi bi-twitter"></i></a></li>
+          <li className="ms-3"><a href="https://discord.gg/nhqyng5wQ9"><i className="bi bi-discord"></i></a></li>
         </ul>
       </footer>
       </>
