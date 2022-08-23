488837f19d0c4a96e1aaa26a948a63ebd9241d15
danny de <danielde666@users.noreply.github.com>
2022-08-23 11:53:09 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,9 @@ export default function Home() {
     <div>
       <Head>
 	      <title>Serving The People - The Social Platform of the Future, Today</title>	<meta name="description" content="The Social Platform of the Future, Today" />
+        
+        <meta charset="UTF-8"></meta>
+        <meta name="viewport" content="width=990"></meta>
         <meta property="og:locale" content="en_US" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Serving The People" />
