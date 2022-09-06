980f9f0cf3a6303d14d873b2f1e9f4a10609a711
danny de <danielde666@users.noreply.github.com>
2022-09-06 10:08:49 -0700

    logo tweak mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -104,7 +104,7 @@ const Home: FC<PageProps> = ({ discordMemberCount, seedsSupply }) => {
             <Link href="/">
             <a className="navbar-brand">
             <Image
-            className="bi me-1"
+            className="bi me-1 logo"
             width={40}
             height={40}
             src="/IMG_5549.png"
