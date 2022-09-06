403db007ea1f8f2ea903343b3848cf076fae9b06
danny de <danielde666@users.noreply.github.com>
2022-09-06 13:50:46 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,7 +58,8 @@ const Home: FC<PageProps> = ({ discordMemberCount, seedsSupply }) => {
           content="The Social Platform of the Future, Today"
         />
         <meta charSet="UTF-8"></meta>
-        <meta name="viewport" content="width=600"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
         <meta property="og:locale" content="en_US" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Serving The People" />
