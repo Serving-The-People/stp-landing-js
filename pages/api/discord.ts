a99f76101c9ce6c952e77d086f3232de545d1a4e
Sam Grund <samgrund@Sams-MBP.fios-router.home>
2022-09-19 12:53:52 -0400

    fix: handle discord login within rest handler

--- pages/api/discord.ts
+++ pages/api/discord.ts
@@ -4,10 +4,8 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
 });
 
-const loginPromise = client.login(process.env.NODE_DISCORD_BOT_TOKEN);
-
 const handler: NextApiHandler<{ count: string }> = async (req, res) => {
-  await loginPromise;
+  await client.login(process.env.NODE_DISCORD_BOT_TOKEN);
   const guild = client.guilds.cache.get("822259948586270770");
   const members = await guild?.members.fetch();
   const guildMemberCount = members?.filter((member) => !member.user.bot).size;
