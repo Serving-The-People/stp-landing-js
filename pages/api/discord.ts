d8fac35b5885402fea0ec2baeffb6bc9bc7f990f
Sam Grund <samgrund@Sams-MBP.fios-router.home>
2022-09-21 15:09:29 -0400

    chore: wrap discord in try catch

--- pages/api/discord.ts
+++ pages/api/discord.ts
@@ -6,17 +6,21 @@ const client = new Client({
 });
 
 const handler: NextApiHandler<{ count: string }> = async (req, res) => {
-  await client.login(process.env.NODE_DISCORD_BOT_TOKEN);
-
-  const guild = client.guilds.cache.get("822259948586270770");
-
-  // this is an expensive operation, skip for now
-  // difference is it filters out bots, but there aren't that many in our server
-  // const members = await guild?.members.fetch();
-  // const guildMemberCount = members?.filter((member) => !member.user.bot).size;
-  res.status(200).json({
-    count: `${guild?.memberCount || ""}`,
-  });
+  try {
+    await client.login(process.env.NODE_DISCORD_BOT_TOKEN);
+    const guild = client.guilds.cache.get("822259948586270770");
+    // this is an expensive operation, skip for now
+    // difference is it filters out bots, but there aren't that many in our server
+    // const members = await guild?.members.fetch();
+    // const guildMemberCount = members?.filter((member) => !member.user.bot).size;
+    res.status(200).json({
+      count: `${guild?.memberCount || ""}`,
+    });
+  } catch {
+    res.status(200).json({
+      count: "",
+    });
+  }
 };
 
 export default handler;
