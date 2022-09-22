cfb332c94f7fc33c6a008e025eb4b333e656318a
Sam Grund <samgrund@Sams-MBP.fios-router.home>
2022-09-22 00:11:35 -0400

    chore: debugging

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,15 @@ import { providers } from "ethers";
 import { WagmiConfig, createClient, configureChains, chain } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
-
-import { InjectedConnector } from "wagmi/connectors/injected";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
 import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
 import { AppProps } from "next/app";
 
-const alchemyKey = process.env.NODE_ALCHEMY_KEY;
-
 const { provider, webSocketProvider } = configureChains(
   [chain.mainnet],
   [
-    alchemyProvider({ apiKey: alchemyKey, priority: 0 }),
+    alchemyProvider({ apiKey: process.env.NODE_ALCHEMY_KEY, priority: 0 }),
     publicProvider({ priority: 1 }),
   ]
 );
@@ -25,19 +22,19 @@ const client = createClient({
   provider,
   webSocketProvider,
   connectors: [
-    new InjectedConnector({ options: { name: "MetaMask" } }),
+    new MetaMaskConnector(),
     new WalletConnectConnector({
       options: {
         qrcode: true,
         rpc: {
-          1: `https://eth-mainnet.alchemyapi.io/v2/${alchemyKey}`,
+          1: `https://eth-mainnet.alchemyapi.io/v2/${process.env.NODE_ALCHEMY_KEY}`,
         },
       },
     }),
     new CoinbaseWalletConnector({
       options: {
-        appName: "stp-docs",
-        jsonRpcUrl: `https://eth-mainnet.alchemyapi.io/v2/${alchemyKey}`,
+        appName: "stp-home",
+        jsonRpcUrl: `https://eth-mainnet.alchemyapi.io/v2/${process.env.NODE_ALCHEMY_KEY}`,
       },
     }),
   ],
