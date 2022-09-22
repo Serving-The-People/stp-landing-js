c6abcb7093dc026286f51875727b0215a2108027
Sam Grund <samgrund@Sams-MBP.fios-router.home>
2022-09-21 21:50:31 -0400

    chore: just use discord api

--- pages/api/discord.ts
+++ pages/api/discord.ts
@@ -1,25 +1,19 @@
-import { Client, GatewayIntentBits } from "discord.js";
 import { NextApiHandler } from "next";
-
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
-});
 
 const handler: NextApiHandler<{ count: string }> = async (req, res) => {
   try {
-    await client.login(process.env.NODE_DISCORD_BOT_TOKEN);
-    const guild = client.guilds.cache.get("822259948586270770");
-    // this is an expensive operation, skip for now
-    // difference is it filters out bots, but there aren't that many in our server
-    // const members = await guild?.members.fetch();
-    // const guildMemberCount = members?.filter((member) => !member.user.bot).size;
+    const discordRes = await fetch(
+      `https://discord.com/api/guilds/822259948586270770?with_counts=true`,
+      {
+        headers: { Authorization: `Bot ${process.env.NODE_DISCORD_BOT_TOKEN}` },
+      }
+    );
+    const json = await discordRes.json();
     res.status(200).json({
-      count: `${guild?.memberCount || ""}`,
+      count: `${json.approximate_member_count || ""}`,
     });
   } catch {
-    res.status(200).json({
-      count: "",
-    });
+    res.status(200).json({ count: "" });
   }
 };
 
